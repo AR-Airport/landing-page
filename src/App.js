c478ce1e415087b95df3e5fa0223e05158cf7817
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-23 21:33:01 -0400

    added

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+      <div className="App">
+          <div className="sub-page">
+              <h1>AR Airport</h1>
+          </div>
+      </div>
   );
 }
 
