badea1e8ec0f2ec9de4919472a459b778b0fd76b
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-23 21:58:25 -0400

    added main title

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
+import {  } from '@material-ui/core'
 import './App.css';
 
 function App() {
   return (
       <div className="App">
           <div className="sub-page">
-              <h1>AR Airport</h1>
+              &nbsp;<h1 className="main-title">AR Airport</h1>
           </div>
       </div>
   );
