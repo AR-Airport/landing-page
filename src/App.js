4c0e293496a19cf4215c3702ae9b60b37b157d68
Apinan Yogaratnam <72412733+apinanyogaratnam@users.noreply.github.com>
2021-08-23 22:28:02 -0400

    added image of phone

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import {  } from '@material-ui/core'
 import './App.css';
 
 function App() {
-  return (
-      <div className="App">
-          <div className="sub-page">
-              &nbsp;<h1 className="main-title">AR Airport</h1>
-          </div>
-      </div>
-  );
+    return (
+        <div className="App">
+            <div className="sub-page">
+                &nbsp;<h1 className="main-title">AR Airport</h1>
+                <h4 className="sub-title">Your Personal Augmented Reality GPS</h4>
+                <img src={require("./iphonex.png").default}></img>
+            </div>
+        </div>
+    );
 }
 
 export default App;
